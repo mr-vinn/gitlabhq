7fe0422f844b0b6f342a66e7a711ebff5c68b7c6
Vince Okada <vokada@mrvinn.com>
2014-04-28 12:16:33 -0500

    Namespace observers
    Add observer classes to the Gitlab module.

--- app/observers/gitlab/project_activity_cache_observer.rb
+++ app/observers/gitlab/project_activity_cache_observer.rb
@@ -1,8 +1,10 @@
-class ProjectActivityCacheObserver < BaseObserver
-  observe :event
+module Gitlab
+  class ProjectActivityCacheObserver < BaseObserver
+    observe :event
 
-  def after_create(event)
-    event.project.update_column(:last_activity_at, event.created_at) if event.project
+    def after_create(event)
+      event.project.update_column(:last_activity_at, event.created_at) if event.project
+    end
   end
-end
 
+end
--- app/observers/gitlab/users_group_observer.rb
+++ app/observers/gitlab/users_group_observer.rb
@@ -1,9 +1,11 @@
-class UsersGroupObserver < BaseObserver
-  def after_create(membership)
-    notification.new_group_member(membership)
-  end
+module Gitlab
+  class UsersGroupObserver < BaseObserver
+    def after_create(membership)
+      notification.new_group_member(membership)
+    end
 
-  def after_update(membership)
-    notification.update_group_member(membership) if membership.group_access_changed?
+    def after_update(membership)
+      notification.update_group_member(membership) if membership.group_access_changed?
+    end
   end
 end
