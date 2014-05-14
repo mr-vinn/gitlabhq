83f9c08bbe598c566832a9bf8cab341d44cc120a
Vince Okada <vokada@mrvinn.com>
2014-05-14 09:43:50 -0500

    Move require() call
    Require Devise's ORM library from 2_app.rb instead of devise.rb.  This
    is necessary to avoid errors when loading the files in lib/, which is
    done from 2_app.rb.

--- config/initializers/2_app.rb
+++ config/initializers/2_app.rb
@@ -1,5 +1,14 @@
 require 'gitlab/popen'
 
+# ==> Devise ORM configuration
+# Load and configure the ORM. Supports :active_record (default) and
+# :mongoid (bson_ext recommended) by default. Other ORMs may be
+# available as additional gems.
+#
+# This is normally set in the Devise initializer, but it's included here so
+# that libs can be loaded.
+require 'devise/orm/active_record'
+
 module Gitlab
   VERSION = File.read(Rails.root.join("VERSION")).strip
   REVISION = Gitlab::Popen.popen(%W(git log --pretty=format:%h -n 1)).first.chomp
