82e1636001a004452daf78396f277541bde681d1
Vince Okada <vokada@mrvinn.com>
2014-05-14 07:32:44 -0500

    Update new files with namespacing
    Update paths and class names in new files from upstream.

--- spec/finders/notes_finder_spec.rb
+++ spec/finders/notes_finder_spec.rb
@@ -1,38 +1,40 @@
 require 'spec_helper'
 
-describe NotesFinder do
-  let(:user) { create :user }
-  let(:project) { create :project }
-  let(:note1) { create :note_on_commit, project: project }
-  let(:note2) { create :note_on_commit, project: project }
-  let(:commit) { note1.noteable }
-
-  before do
-    project.team << [user, :master]
-  end
-
-  describe :execute do
-    let(:params)  { { target_id: commit.id, target_type: 'commit', last_fetched_at: 1.hour.ago.to_i } }
+module Gitlab
+  describe NotesFinder do
+    let(:user) { create :user }
+    let(:project) { create :project }
+    let(:note1) { create :note_on_commit, project: project }
+    let(:note2) { create :note_on_commit, project: project }
+    let(:commit) { note1.noteable }
 
     before do
-      note1
-      note2
+      project.team << [user, :master]
     end
 
-    it 'should find all notes' do
-      notes = NotesFinder.new.execute(project, user, params)
-      notes.size.should eq(2)
-    end
+    describe :execute do
+      let(:params)  { { target_id: commit.id, target_type: 'commit', last_fetched_at: 1.hour.ago.to_i } }
 
-    it 'should raise an exception for an invalid target_type' do
-      params.merge!(target_type: 'invalid')
-      expect { NotesFinder.new.execute(project, user, params) }.to raise_error('invalid target_type')
-    end
+      before do
+        note1
+        note2
+      end
 
-    it 'filters out old notes' do
-      note2.update_attribute(:updated_at, 2.hours.ago)
-      notes = NotesFinder.new.execute(project, user, params)
-      notes.should eq([note1])
+      it 'should find all notes' do
+        notes = NotesFinder.new.execute(project, user, params)
+        notes.size.should eq(2)
+      end
+
+      it 'should raise an exception for an invalid target_type' do
+        params.merge!(target_type: 'invalid')
+        expect { NotesFinder.new.execute(project, user, params) }.to raise_error('invalid target_type')
+      end
+
+      it 'filters out old notes' do
+        note2.update_attribute(:updated_at, 2.hours.ago)
+        notes = NotesFinder.new.execute(project, user, params)
+        notes.should eq([note1])
+      end
     end
   end
 end
