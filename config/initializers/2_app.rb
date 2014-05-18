3d3d554598768bff3435c5a6bf0abf5b20c2e1ef
Vince Okada <vokada@mrvinn.com>
2014-05-18 11:35:03 -0500

    Don't set VERSION in initializer
    `Gitlab::VERSION` is set in the gemspec file now, so don't set it again
    in the app initializer.

--- config/initializers/2_app.rb
+++ config/initializers/2_app.rb
@@ -10,7 +10,6 @@ require 'gitlab/popen'
 require 'devise/orm/active_record'
 
 module Gitlab
-  VERSION = File.read(Rails.root.join("VERSION")).strip
   REVISION = Gitlab::Popen.popen(%W(git log --pretty=format:%h -n 1)).first.chomp
 
   def self.config
