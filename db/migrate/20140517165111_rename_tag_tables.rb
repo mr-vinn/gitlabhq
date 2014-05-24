1d66ae63916dfd77bb325862f7ee8cb83b66768b
Vince Okada <vokada@mrvinn.com>
2014-05-24 13:48:03 -0500

    Squash migrations
    Delete old migrations and create a new init_schema migration so that
    engine users have fewer files to copy and run.

--- db/migrate/20140517165111_rename_tag_tables.rb
+++
@@ -1,7 +0,0 @@
-class RenameTagTables < ActiveRecord::Migration
-  def change
-    rename_table :gitlab_tags, :tags
-    rename_table :gitlab_taggings, :taggings
-    rename_index :taggings, :index_taggings_on_taggable_id_taggable_type_context, :index_taggings_on_taggable_id_and_taggable_type_and_context
-  end
-end
