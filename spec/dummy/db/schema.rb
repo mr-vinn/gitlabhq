ff1f2efcd466b48274ec21ba22b8eef06f18da1b
Vince Okada <vokada@mrvinn.com>
2014-05-27 10:58:57 -0500

    Update dummy schema
    Update the schema version number in the dummy app after running the
    squashed migration.

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523020220) do
+ActiveRecord::Schema.define(version: 20140524184438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
