91c7f604ffa915d69001141aa0616111f3086041
Vince Okada <vokada@mrvinn.com>
2014-05-27 15:00:38 -0500

    Copy more config files
    Copy the config/*.example* files during engine installation.

--- lib/generators/gitlab/install_generator.rb
+++ lib/generators/gitlab/install_generator.rb
@@ -4,8 +4,13 @@ module Gitlab
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../../../../config", __FILE__)
 
-    def copy_config
+    def copy_config_examples
+      copy_file "aws.yml.example", "config/aws.yml"
       copy_file "gitlab.yml.example", "config/gitlab.yml"
+      copy_file "resque.yml.example", "config/resque.yml"
+      copy_file "unicorn.rb.example", "config/unicorn.rb"
+      copy_file "unicorn.rb.example.development", "config/unicorn.rb.example.development"
+    end
     end
   end
 end
