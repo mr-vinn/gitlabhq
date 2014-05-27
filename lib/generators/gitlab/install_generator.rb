6c00bf18362445bfd6bf5c907a1a54038dc5ec97
Vince Okada <vokada@mrvinn.com>
2014-05-27 15:14:15 -0500

    Copy database config
    Apply the database.yml.erb template when installing the engine, and add
    some private methods that the template can use to get the application
    name and database adapter name.

--- lib/generators/gitlab/install_generator.rb
+++ lib/generators/gitlab/install_generator.rb
@@ -4,6 +4,12 @@ module Gitlab
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../../../../config", __FILE__)
 
+    argument :db_type, :type => :string, :default => "postgresql", :desc => "Specify the database type.  Valid database types are 'postgresql' (default) or 'mysql'", :banner => "database type"
+
+    def copy_db_config
+      template "database.yml.erb", "config/database.yml"
+    end
+
     def copy_config_examples
       copy_file "aws.yml.example", "config/aws.yml"
       copy_file "gitlab.yml.example", "config/gitlab.yml"
@@ -11,6 +17,19 @@ module Gitlab
       copy_file "unicorn.rb.example", "config/unicorn.rb"
       copy_file "unicorn.rb.example.development", "config/unicorn.rb.example.development"
     end
+
+    private
+
+    def app_name
+      Rails.application.class.parent_name.underscore
+    end
+
+    def adapter_name
+      case db_type
+      when 'postgresql' then :postgresql
+      when 'mysql' then :mysql2
+      else raise("Unsupported database type '#{db_type}'")
+      end
     end
   end
 end
